3e7f7d550a7fa93fe4dfe1ee5a1a925bc8828b1a
seonghun.han <hsh931212a@naver.com>
2022-04-04 16:51:40 +0900

    coding test base on Python

---
+++ 기본/9498Grade.py
@@ -0,0 +1,8 @@
+A = input()
+A = int(A)
+
+if 90<=A<=100 : print("A")
+elif 80<=A<90 : print("B")
+elif 70<=A<80 : print("C")
+elif 60<=A<70 : print("D")
+else : print("F")
---
+++ 기본/basic.py
@@ -0,0 +1,7 @@
+
+
+number = list(map(int, input().split()))
+
+number.sort()
+
+print(number[0],number[1],number[2])

049cccfb603e2064a9d64a1d044255a46bf50254
seonghun.han <hsh931212a@naver.com>
2022-06-06 15:47:35 +0900

    asd

--- 7.DP/2630.py
+++ 7.DP/2630.py
@@ -1,26 +1,61 @@
 import sys
 
 N = int(sys.stdin.readline())
-paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
 
-result = []
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range (N)]
+zero = []
+one = []
 
-def solution(x, y, N) :
-  color = paper[x][y]
-  for i in range(x, x+N) :
-    for j in range(y, y+N) :
-      if color != paper[i][j] :
-        solution(x, y, N//2)
-        solution(x, y+N//2, N//2)
-        solution(x+N//2, y, N//2)
-        solution(x+N//2, y+N//2, N//2)
-        return
-  if color == 0 :
-    result.append(0)
-  else :
-    result.append(1)
+def recursion(x,y,N) :
+      
+      check_point = arr[x][y]
+
+      for i in range (x, x+N) :
+            for j in range (y, y+N) :
+                  
+                  if check_point != arr[i][j] :
+                        recursion(x, y, N // 2)
+                        recursion(x, y + N // 2, N // 2)
+                        recursion(x + N // 2, y, N // 2)
+                        recursion(x + N // 2, y + N // 2, N // 2)
+                        return
+      if check_point == 0 :
+            zero.append(0)
+      else :
+            one.append(1)
+
+recursion(0,0,N)
+print(len(zero))
+print(len(one))
 
 
-solution(0,0,N)
-print(result.count(0))
-print(result.count(1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

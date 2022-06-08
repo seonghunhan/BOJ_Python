b0c2323dbdf6e276992346fa23c85178d4bd9081
seonghun.han <hsh931212a@naver.com>
2022-06-08 14:32:38 +0900

    asd

---
+++ 9.백트래킹/15649.py
@@ -0,0 +1,21 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+arr = []
+
+def dfs() :
+
+    if len(arr) == m :
+        print(' '.join(map(str, arr)))
+        return
+
+    for i in range(1,n+1) :
+        
+        if i not in arr :
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+
+dfs()

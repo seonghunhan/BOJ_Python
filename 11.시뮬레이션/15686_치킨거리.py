2c2f3d31a727e360d7804884df0758405c2bf251
seonghun.han <hsh931212a@naver.com>
2022-10-04 20:02:39 +0900

    asd

--- 11.시뮬레이션/15686_치킨거리.py
+++ 11.시뮬레이션/15686_치킨거리.py
@@ -3,9 +3,7 @@ from itertools import combinations
 
 N, M = map(int, sys.stdin.readline().split())
 
-
 city = list(list(map(int, sys.stdin.readline().split())) for _ in range(N))
-
 
 chickStore = []
 house = []
@@ -16,15 +14,21 @@ for i in range(N) :
             house.append([i+1,j+1])
         if city[i][j] == 2 :
             chickStore.append([i+1,j+1])
-result = 99999            
+result = 99999
+
+# chickStore에서 M개 조합으로 이뤄진 리스트 1개씩 chi로 보낸다            
 for chi in combinations(chickStore, M) :
     chi_len = 0
+    # 하우스를 1개씩 보낸다
     for hou in house :
         temp = 99999
+        # 하우스 1개와 M개 조합으로 이뤄진 치킨집 1개씩 비교하여 최솟값(치킨거리) 뽑는다
+        # 치킨거리는 한 집에서 여러 치킨집과의 거리중 가장 작은값!
         for k in range(M) :
-            temp = min(temp, abs(chi[k][0] - hou[0]) + abs(chi[k][1] - hou[1])) 
+            temp = min(temp, abs(chi[k][0] - hou[0]) + abs(chi[k][1] - hou[1]))
+        # 치킨거리는 chi_len에 더해준다 
         chi_len += temp  
-        
+    # 다 더해준 치킨거리(도시 치킨거리)의 최솟값을 찾는다    
     result = min(result, chi_len)    
         
 print(result)
--- 11.시뮬레이션/17281_야구공(남의코드참고).py
+++ 11.시뮬레이션/17281_야구공(남의코드참고).py
@@ -4,18 +4,15 @@ input = sys.stdin.readline
 
 N = int(input())
 innings = [list(map(int, input().split())) for _ in range(N)]
-
 result = 0
 
 def simulation(lineupList) :
     
-
-
     Runner = 0
     score = 0
     for currentInning in innings :
         b1, b2, b3 = 0, 0, 0        
-        outCount = 0        
+        outCount = 0
         while outCount < 3 :
             if currentInning[lineupList[Runner]] == 0 :
                 outCount += 1
@@ -31,18 +28,13 @@ def simulation(lineupList) :
             elif currentInning[lineupList[Runner]] == 4 :
                 score += b1 + b2 + b3 + 1
                 b1, b2, b3 = 0, 0, 0
-            
             Runner = (Runner + 1) % 9
-    
     return score
         
-    
-    
-    
-
+# 각 이닝에서 뛸 번호를 순열 경우의 수 만들기    
 for lineupList in permutations(range(1,9), 8) :
+    # 1번타자는 4번타자 고정
     lineupList = list(lineupList[:3]) + [0] + list(lineupList[3:])
-    
     result = max(result, simulation(lineupList))
     
 print(result)

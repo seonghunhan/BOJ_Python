6716c3cde92b50d9c03b8eaf1f461107e5cb4f50
seonghun.han <hsh931212a@naver.com>
2023-02-09 15:52:05 +0900

    added 평범한배낭(DP 2차원)

---
+++ 07.DP/12865_평범한배낭.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+stuff = [ [0,0]]
+
+for i in range(N) :
+    stuff.append(list(map(int, input().split())))
+    
+board = [ list(0 for _ in range(K+1)) for _ in range(N+1)]
+
+
+for i in range(1, N+1) :
+    
+    weight = stuff[i][0]
+    value = stuff[i][1]
+    
+    for j in range(1, K+1) :
+        
+        if weight > j :
+            board[i][j] = board[i-1][j]
+        
+        else :
+             maxValue = max(value + board[i-1][j-weight] , board[i-1][j])
+             board[i][j] = maxValue
+             
+print(board[-1][-1])
